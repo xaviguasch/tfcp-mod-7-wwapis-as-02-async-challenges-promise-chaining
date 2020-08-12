ec4db2820945ca9ec38432b3e7e12ab21335de35
xaviguasch <xaviguasch@gmail.com>
2020-08-12 11:57:07 +0200

    refactored previous code using async/await

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 // document.getElementById('action').addEventListener('click', () => {
 //   const userApi = 'https://randomuser.me/api/'
 
-// call user API
-// store random first name in a variable firstUser
-// call user API again, after you finish calling first one
-// store random first name in variable secondUser
-// console log `${firstUser} and ${secondUser} are friends`
+//   // call user API
+//   // store random first name in a variable firstUser
+//   // call user API again, after you finish calling first one
+//   // store random first name in variable secondUser
+//   // console log `${firstUser} and ${secondUser} are friends`
 
-// const fetchCall = async () => {
-// const fetchedApi = await fetch(userApi)
-// const fetchedJson = await fetchedApi.json()
-// const firstUser = fetchedJson.results[0].name.first
-// const fetchedApi2 = await fetch(userApi)
-// const fetchedJson2 = await fetchedApi2.json()
-// const secondUser = fetchedJson2.results[0].name.first
-// console.log(`${firstUser} and ${secondUser} are friends`)
-//   }
+//   fetch(userApi)
+//     .then((response) => response.json())
+//     .then((response) => {
+//       const firstUser = response.results[0].name.first
+
+//       fetch(userApi)
+//         .then((response) => response.json())
+//         .then((response) => {
+//           const secondUser = response.results[0].name.first
+//           console.log(`${firstUser} and ${secondUser} are friends`)
+//         })
+//     })
 // })
 
 document.getElementById('action').addEventListener('click', () => {
@@ -27,16 +30,15 @@ document.getElementById('action').addEventListener('click', () => {
   // store random first name in variable secondUser
   // console log `${firstUser} and ${secondUser} are friends`
 
-  fetch(userApi)
-    .then((response) => response.json())
-    .then((response) => {
-      const firstUser = response.results[0].name.first
+  const fetchCall = async () => {
+    const fetchedApi = await fetch(userApi)
+    const fetchedJson = await fetchedApi.json()
+    const firstUser = fetchedJson.results[0].name.first
+    const fetchedApi2 = await fetch(userApi)
+    const fetchedJson2 = await fetchedApi2.json()
+    const secondUser = fetchedJson2.results[0].name.first
+    console.log(`${firstUser} and ${secondUser} are friends`)
+  }
 
-      fetch(userApi)
-        .then((response) => response.json())
-        .then((response) => {
-          const secondUser = response.results[0].name.first
-          console.log(`${firstUser} and ${secondUser} are friends`)
-        })
-    })
+  fetchCall()
 })
